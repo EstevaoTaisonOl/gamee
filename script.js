26ca802c7b72373934cce98975e500ae3a7193cd
EstevaoTaisonOl <167809169+EstevaoTaisonOl@users.noreply.github.com>
2024-08-14 23:05:00 -0300

    funcoes arrumadas
    falta  aprimorar o sistema de game

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+
 var player = undefined
 var game = document.getElementById("game")
 
 var p = false
+var partida = false
 
 const socket = new WebSocket('ws://127.0.0.1:8080');
 
@@ -38,6 +40,22 @@ function verificacaoTela(fun){
     }
 }
 
+function verficarPegarFruit(){
+    if(document.getElementById("fruit")){
+        if(player.style.left == document.getElementById("fruit").style.left){
+            if(player.style.top == document.getElementById("fruit").style.top){
+                var info = {
+                    info: "frutaPega",
+                    id: player.id,
+                }
+                socket.send(JSON.stringify(info))
+            }
+        }else{
+            console.log("Nao")
+        }
+    }
+}
+
 function move(direcao) {
     var move = 0
     var verificacao = verificacaoTela(direcao)
@@ -59,8 +77,28 @@ function move(direcao) {
             player.style.top = (currentLeft + move) + "px";
         }
     }
+
+    var info = {
+        info: "atualizarLocalJogadores",
+        left: player.style.left,
+        top: player.style.top,
+        id: player.id
+    }
+    verficarPegarFruit()
+    socket.send(JSON.stringify(info))
 }
 
+function spawnFruit(left,top){
+    var fruit = document.createElement("div")
+
+    fruit.id = "fruit"
+    fruit.classList = "fruit"
+
+    fruit.style.left = left
+    fruit.style.top = top
+
+    game.appendChild(fruit)
+}
 
 document.body.addEventListener("keydown", ({key}) => {
     if(key === "d"){
@@ -80,37 +118,144 @@ document.body.addEventListener("keydown", ({key}) => {
     }
 });
 
+var buttonPronto = document.getElementById("button-pronto")
+
+buttonPronto.addEventListener("click", () => {
+    if(buttonPronto.className == "nao"){
+        buttonPronto.className = "pronto"
+        var info = {
+            info: "atualizarStatsPronto",
+            acont: 1,
+        }
+        socket.send(JSON.stringify(info))
+    }else{
+        buttonPronto.className = "nao"
+        var info = {
+            info: "atualizarStatsPronto",
+            acont: -1,
+        }
+        socket.send(JSON.stringify(info))
+    }
+})
+
+function addPlayerList(id){
+    var trB = document.getElementById("tr-tbody")
+
+    var tr = document.createElement("tr")
+    tr.id = `tr-${id}`
+                
+    var th = document.createElement("th")
+    th.innerHTML = id
+
+    var thP = document.createElement("th")
+    thP.innerHTML = 0
+    thP.id = `table-${id}`
+
+    tr.appendChild(th)
+    tr.appendChild(thP)
+
+    trB.appendChild(tr)
+}
+
 
 socket.onmessage = async function(event) {
     var data = JSON.parse(event.data)
 
     if(data.info == "addPlayer"){
-        console.log(data.player)
-        console.log(data.id)
-        console.log(data.rep2)
-        if(p == false){
-            var div = document.createElement("div")
-            div.id = `player-${data.id}`
-            div.className = "player"
-
-            game.appendChild(div)
-
-            player = document.getElementById(`player-${data.id}`)
-            p = true
-        }
-        
-            for(var i =0; i < data.rep - 1; i++){
+        for(var i = 0; i < data.variaveis.length; i++){
+            if(i % 2 == 0){
                 var div = document.createElement("div")
-                div.id = `player-${data.rep2[data.ws]}`
-                div.className = "player"
-
+                if(i === 0){
+                    div.id = data.variaveis[data.ind]
+                    player = div
+                    div.className = "player controlado"
+                }else{
+                    div.id = data.variaveis[i - 1]
+                    div.className = "player"
+                }
                 game.appendChild(div)
+                addPlayerList(div.id)
+                if(i === 0){
+                    
+                }
             }
-    }else if(data.info == "removePlayer"){
-        console.log(data.id)
-        var div = document.getElementById(`player-${data.id -1}`)
+        }
+     }else if(data.info == "removePlayer"){
+        var div = document.getElementById(`${data.id}`)
+        var tr = document.getElementById(`tr-${data.id}`)
 
         div.remove()
+        tr.remove()
     }
 
+    if(data.info == "addPlayerExist"){
+        var div = document.createElement("div")
+        div.className = "player"
+
+        div.id = `${data.id}`
+
+        game.appendChild(div)
+        addPlayerList(div.id)
+    }
+
+    if(data.info == "atualizarPlayers"){
+        var number = document.getElementById("number-prontos")
+        var numberS = number.innerHTML.split("/")
+
+        numberS[1] = data.size
+
+
+        number.innerHTML = numberS[0] + "/" + numberS[1]
+    }
+    if(data.info == "atualizarStatsPronto"){
+        var number = document.getElementById("number-prontos")
+        var numberS = number.innerHTML.split("/")
+
+        var n = Number(numberS[0]) + data.acont
+        numberS[0] = n
+
+        number.innerHTML = numberS[0] + "/" + numberS[1]
+        if(numberS[0] == numberS[1]){
+            var nT = 50 * Math.floor(Math.random() * 12) + "px"
+            var nL = 50 * Math.floor(Math.random() * 16) + "px"
+            var info = {
+                info: "IniciarJogo",
+                top: nT,
+                left: nL,
+            }
+            socket.send(JSON.stringify(info))
+        }
+    }
+
+    if(data.info == "atualizarLocalJogadores"){
+        var div = document.getElementById(data.id)
+
+        div.style.top = data.top
+        div.style.left = data.left
+    }
+
+    if(data.info == "attPlayerNewPronto"){
+        var n = 0
+        var number = document.getElementById("number-prontos")
+        var numberS = number.innerHTML.split("/")
+
+        n = Number(numberS[0]) + data.pro
+        if(data.pro == 0){
+            n = 0
+        }
+
+        number.innerHTML = n + "/" + numberS[1]
+    }
+    if(data.info == "spawnFruit"){
+        if(!document.getElementById("fruit")){
+            spawnFruit(data.left, data.top)
+        }
+    }
+
+    if(data.info == "frutaPega"){
+        var table = document.getElementById(`table-${data.id}`)
+        var n = Number(table.innerHTML) + 1
+        table.innerHTML = n
+        document.getElementById("fruit").remove()
+    }
 }
--- server.js
+++ server.js
@@ -4,50 +4,159 @@ const WebSocket = require('ws');
 const server = new WebSocket.Server({ port: 8080 });
 
 const clients = new Set();
-var variaveis = {}
+var variaveis = []
+
+var statsPartida = false
+var fruitSpawn = false
+
+var prontos = []
+
+function pesquisa(fruit,name) {
+  return fruit === name;
+}
+
+function spawnFruit(){
+
+}
 
 server.on('connection', (ws) => {
+  const uniqueId = 'id-' + Date.now();
+
   console.log('Cliente conectado');
   clients.add(ws);
+  var info = {
+    info: "attPlayerNewPronto",
+    pro: prontos,
+  }
+        
+  ws.send(JSON.stringify(info))
   for (const client of clients) {
     if (client.readyState === WebSocket.OPEN) {
-        const uniqueId = 'id-' + Date.now();
-        variaveis[ws] = uniqueId
-        var info = {
-            info: "addPlayer",
-            player: 1,
-            id: uniqueId,
-            rep: clients.size,
-            rep2: variaveis,
-            ws: ws,
-        }
-        client.send(JSON.stringify(info));
+      var info = {
+        info: "atualizarPlayers",
+        size: clients.size,
+        stats: "adicionado"
+      }
+      client.send(JSON.stringify(info));
     }
   }
+  for(var i = 0; i< variaveis.length; i++){
+    clients.forEach(value => {
+      console.log(pesquisa(variaveis[i],value))
+      if(pesquisa(variaveis[i], value)){
+        var info = {
+          info: "addPlayerExist",
+          player: 1,
+          id: uniqueId,
+        }
+
+        value.send(JSON.stringify(info))
+      }
+    });
+  }
+  variaveis.push(ws)
+  variaveis.push(uniqueId)
+  console.log(uniqueId)
+  var ind = undefined
+
+  for (var i = 0; i < variaveis.length; i++) {
+    if (pesquisa(variaveis[i], ws)) {
+        // Use findIndex with a condition function to find the index
+        var ind = variaveis.findIndex(item => item === variaveis[i + 1]);
+        console.log(ind);
+    }
+}
+
+
+  var info = {
+    info: "addPlayer",
+    size: clients.size,
+    variaveis: variaveis,
+    ind: ind
+  }
+
+  ws.send(JSON.stringify(info))
+  
   
   ws.on('message', (message) => {
     const data = message.toString('utf8');
     console.log('Mensagem recebida:', data);
-    // Envie a mensagem para todos os clientes
+    var play = JSON.parse(data)
+        if(play.info == "IniciarJogo"){
+          statsPartida = true
+        }   
+    var dd = JSON.parse(data)
+    if(dd.info == "atualizarStatsPronto"){
+      prontos.push(ws)
+      prontos.push(dd.acont)
+    }
+
     for (const client of clients) {
       if (client.readyState === WebSocket.OPEN) {
+        if(dd.info == "IniciarJogo"){
+            var info = {
+              info: "spawnFruit",
+              left : dd.left,
+              top: dd.top,
+            }
+
+            client.send(JSON.stringify(info))
+        }
         client.send(data);
       }
     }
   });
   ws.on('close', () => {
     console.log('Cliente desconectado');
-    for (const client of clients) {
-        if (client.readyState === WebSocket.OPEN) {
-            var info = {
-                info: "removePlayer",
-                id: variaveis[ws]
-            }
-            client.send(JSON.stringify(info))
-            delete variaveis[ws]
-        }
-    }
     clients.delete(ws);
+    for(var i = 0; i< variaveis.length; i++){
+      clients.forEach(value => {
+        if(pesquisa(variaveis[i], ws)){
+          var info = {
+            info: "removePlayer",
+            player: 1,
+            id: variaveis[i + 1],
+          }
+          value.send(JSON.stringify(info))
+        }
+      });
+    }
+    console.log(prontos)
+    variaveis.splice(variaveis.indexOf(ws), 2);
+    prontos.splice(prontos.indexOf(ws), 2);
+    for(var i = 0; i< prontos.length; i++){
+      clients.forEach(value => {
+        if(pesquisa(prontos[i], ws)){
+          var info = {
+            info: "attPlayerNewPronto",
+            pro: prontos[i + 1],
+          }
+          value.send(JSON.stringify(info))
+          var info = {
+            info: "atualizarPlayers",
+            size: clients.size,
+            stats: "adicionado"
+          }
+          value.send(JSON.stringify(info));
+        }
+      });
+    }
+
+    if(prontos.length == 0){
+      clients.forEach(value => {
+          var info = {
+            info: "attPlayerNewPronto",
+            pro: 0,
+          }
+          value.send(JSON.stringify(info))
+          var info = {
+            info: "atualizarPlayers",
+            size: clients.size,
+            stats: "adicionado"
+          }
+          value.send(JSON.stringify(info));
+      });
+    }
   });
 
   ws.on('error', (error) => {
